87b1fdf45f4f924351c443639062bc9405740670
20170375 <62216628+20170375@users.noreply.github.com>
2022-06-18 15:14:52 +0900

    clean code
    + add debug param in each method (print time cost if 1)

--- django-server/ssummary_site/modules/Converters.py
+++ django-server/ssummary_site/modules/Converters.py
@@ -1,6 +1,8 @@
 from .Translaters import *
 from .Summarizers import *
 
+import time
+
 from hanspell import spell_checker
 
 
@@ -19,7 +21,7 @@ class MyConverter:
         print("Log: summarizor 초기화 성공")
         print("=" * 50, end="\n\n")
 
-    def translate(self, text, target, input_size=5000):
+    def translate(self, text, target, input_size=5000, debug=0):
         """
         Translate the text into Korean by dividing it into chunks.
 
@@ -27,17 +29,24 @@ class MyConverter:
             text (str): Text to be translated
             target (str): Target language
             input_size (int): Size of each chunk
+            debug (int): Debug level
         """
-        return self.translater.translate(text, target, input_size=input_size)
+        start = time.time()
+        result = self.translater.translate(text, target, input_size=input_size)
+        if debug == 1:
+            print("번역 소요시간:", time.time() - start)
+        return result
 
-    def spell_check(self, text, input_size=500):
+    def spell_check(self, text, input_size=500, debug=0):
         """
         Check the spelling of the text.
 
         Args:
             text (str): Text to be checked
             input_size (int): Size of each chunk
+            debug (int): Debug level
         """
+        start = time.time()
 
         result = ""
         dumps = divide(text, input_size=input_size)
@@ -46,11 +55,14 @@ class MyConverter:
             try:
                 result += spell_checker.check(dump).checked
             except Exception as e:
-                    print("ERROR from spell_check(): \n" + dump)
-                    raise e
+                print("ERROR from spell_check(): \n" + dump)
+                raise e
+
+        if debug == 1: 
+            print("검사 소요시간:", time.time() - start)
         return result
 
-    def summarize(self, text, input_size=1024, deep=False):
+    def summarize(self, text, input_size=1024, deep=False, debug=0):
         """
         Summarize the text by dividing it into chunks.
 
@@ -58,5 +70,10 @@ class MyConverter:
             text (str): Text to be translated
             input_size (int): Size of each chunk
             deep (bool): Whether to use deep summarization
+            debug (int): Debug level
         """
-        return self.summarizer.generate(text, input_size=input_size, deep=deep)
+        start = time.time()
+        result = self.summarizer.generate(text, input_size=input_size, deep=deep)
+        if debug == 1: 
+            print("깊은" if deep == True else "얕은", "요약 소요시간:", time.time() - start)
+        return result
--- django-server/ssummary_site/views.py
+++ django-server/ssummary_site/views.py
@@ -13,30 +13,13 @@ def ssummary(request):
         deep = True if request.POST['deep'] == 'true' else False
         target_lang = request.POST['target_lang']
 
-        ## ---시간측정--- ##
-        import time
-        start = time.time()
-        # 번역
-        text_kor = converter.translate(text, 'ko', input_size=5000)
-        print("\n번역 소요시간:", time.time() - start)
-
-        start = time.time()
-        # 문법 검사
-        text_checked_kor = converter.spell_check(text_kor)
-        print("검사 소요시간:", time.time() - start)
-
-        start = time.time()
-        # 요약
-        text_sum = converter.summarize(text_checked_kor, deep=deep)
-        if deep == True: print("깊은 ", end="")
-        else: print("얕은 ", end="")
-        print("요약 소요시간:", time.time() - start)
-
-        start = time.time()
-        # 번역
-        text_res = converter.translate(text_sum, target_lang, input_size=5000)
-        print("번역 소요시간:", time.time() - start, "\n")
-        ## ---시간측정--- ##
+        debug = 1
+        if debug == 1:
+            print()
+        text_kor = converter.translate(text, 'ko', input_size=5000, debug=debug) # 번역
+        text_checked_kor = converter.spell_check(text_kor, debug=debug) # 문법 검사
+        text_sum = converter.summarize(text_checked_kor, deep=deep, debug=debug) # 요약
+        text_res = converter.translate(text_sum, target_lang, input_size=5000, debug=debug) # 번역
 
         # JSON 객체 생성
         result = {
